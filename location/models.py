d46e7b7942d348a66e4a0659706307092a84a366
Ali0011k <kly441781@gmail.com>
2024-04-17 22:00:09 +0330

    fixed country and city search bug

--- location/models.py
+++ location/models.py
@@ -4,8 +4,8 @@ from django.contrib.gis.geos import Point
 
 
 class Place(models.Model):
-    city = models.CharField(max_length=255)
     country = models.CharField(max_length=255, default="")
+    city = models.CharField(max_length=255)
     location = PlainLocationField(
-        based_fields=["city", "country"], zoom=10, default=Point(51.3890, 35.6892)
+        based_fields=["country", "city",], zoom=10, default=Point(51.3890, 35.6892)
     )
