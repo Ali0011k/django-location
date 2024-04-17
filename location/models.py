4d21117e3988780e5a5ee17135a4fbe8e59b38bb
Ali0011k <kly441781@gmail.com>
2024-04-17 20:35:39 +0330

    fixed migrations bug

--- location/models.py
+++ location/models.py
@@ -5,7 +5,7 @@ from django.contrib.gis.geos import Point
 
 class Place(models.Model):
     city = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
+    country = models.CharField(max_length=255, default="")
     location = PlainLocationField(
         based_fields=["city", "country"], zoom=10, default=Point(51.3890, 35.6892)
     )
